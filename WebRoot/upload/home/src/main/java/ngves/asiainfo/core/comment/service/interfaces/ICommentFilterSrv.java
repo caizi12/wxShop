35c000dfa3863bc9d8c9db442e54fb91140a03d8
caizi12 <caizi12@126.com>
2022-10-21 18:26:44 +0800

    Delete WebRoot/upload/home/src/main/java/ngves directory
    dd

--- WebRoot/upload/home/src/main/java/ngves/asiainfo/core/comment/service/interfaces/ICommentFilterSrv.java
+++
@@ -1,8 +0,0 @@
-package ngves.asiainfo.core.comment.service.interfaces;
-
-public interface ICommentFilterSrv {
-	public void setFilterWords(String[] filterWordsInfos);
-	public String doFilter(String comment);
-	public String replaceFilterWords(int i, String comment);
-	
-}
